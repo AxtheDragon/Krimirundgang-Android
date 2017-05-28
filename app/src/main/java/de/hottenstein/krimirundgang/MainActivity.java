6371cef1df236f523152f4a477417ccff41554ee
Andi <ax.the.dragon@googlemail.com>
2017-05-28 16:18:09 +0200

    added missing variable

--- app/src/main/java/de/hottenstein/krimirundgang/MainActivity.java
+++ app/src/main/java/de/hottenstein/krimirundgang/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     public static final String EXTRA_MESSAGE = "de.hottenstein.krimirundgang.MESSAGE";
 
     protected GoogleApiClient mGoogleApiClient;
-    private int MY_PERMISSIONS_REQUEST_LOCATION;
+    private enum MY_PERMISSIONS_REQUEST_LOCATION{RETURN_CODE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,6 +95,31 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     private boolean isAllowedToAccessLocation(String locationGranularity) {
         return ActivityCompat.checkSelfPermission(this, locationGranularity) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
