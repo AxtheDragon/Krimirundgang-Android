c9b04af3a8bb0ee413cf8a251e1684527b67cf8b
Andi <ax.the.dragon@googlemail.com>
2017-05-28 14:57:43 +0200

    added permission check

--- app/src/main/java/de/hottenstein/krimirundgang/MainActivity.java
+++ app/src/main/java/de/hottenstein/krimirundgang/MainActivity.java
@@ -1,9 +1,11 @@
 package de.hottenstein.krimirundgang;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -62,6 +64,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         TextView mLatitudeText = (TextView) findViewById(R.id.LatitudeText);
         TextView mLongitudeText = (TextView) findViewById(R.id.LongitudeText);
 
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
         if (mLastLocation != null) {
