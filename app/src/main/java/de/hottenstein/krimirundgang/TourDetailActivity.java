4ee37af67edd0ea2a0f9dd23578413d256c5414d
Azure7111 <azure.diamond7111@googlemail.com>
2017-07-08 22:07:14 +0200

    Add new and mostly empty TourDetailActivity view

---
+++ app/src/main/java/de/hottenstein/krimirundgang/TourDetailActivity.java
@@ -0,0 +1,13 @@
+package de.hottenstein.krimirundgang;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TourDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tour_detail);
+    }
+}
