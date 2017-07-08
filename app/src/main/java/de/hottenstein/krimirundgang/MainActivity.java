3a508a664f43f35edb67c3816ff1cbfd52bb5ecd
Azure7111 <azure.diamond7111@googlemail.com>
2017-07-08 13:58:46 +0200

    Automatically update location periodically when the app is running

--- app/src/main/java/de/hottenstein/krimirundgang/MainActivity.java
+++ app/src/main/java/de/hottenstein/krimirundgang/MainActivity.java
@@ -2,9 +2,11 @@ package de.hottenstein.krimirundgang;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -12,12 +14,21 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,8 +39,22 @@ public class MainActivity extends AppCompatActivity {
      */
     protected Location mLastLocation;
 
+    /**
+     * Constant used to identify the location settings dialog. Must be positive.
+     */
+    private static final int REQUEST_CHECK_SETTINGS = 1;
+
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    /**
+     * Callback for Location events.
+     */
+    private LocationCallback mLocationCallback;
     private FusedLocationProviderClient mFusedLocationClient;
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private LocationRequest mLocationRequest;
+    private SettingsClient mSettingsClient;
+
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 0;
 
     protected LocationRequest createLocationRequest() {
@@ -46,6 +71,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mSettingsClient = LocationServices.getSettingsClient(this);
+        mLocationCallback = createLocationCallback();
+        mLocationRequest = createLocationRequest();
+        mLocationSettingsRequest = buildLocationSettingsRequest();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
     }
 
     @Override
@@ -55,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
         if (!isPermitted()) {
             requestPermissions();
         } else {
-            getLastLocation();
+            displayLastLocation();
         }
     }
 
@@ -70,6 +116,80 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * Uses a {@link com.google.android.gms.location.LocationSettingsRequest.Builder} to build
+     * a {@link com.google.android.gms.location.LocationSettingsRequest} that is used for checking
+     * if a device has the needed location settings.
+     */
+    private LocationSettingsRequest buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        return builder.build();
+    }
+
+    /**
+     * Creates a callback for receiving location events.
+     */
+    private LocationCallback createLocationCallback() {
+        return new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                mLastLocation = locationResult.getLastLocation();
+                displayLastLocation();
+            }
+        };
+    }
+
+    /**
+     * Requests location updates from the FusedLocationApi. Note: we don't call this unless location
+     * runtime permission has been granted.
+     */
+    private void startLocationUpdates() {
+        // Begin by checking if the device has the necessary location settings.
+        mSettingsClient.checkLocationSettings(mLocationSettingsRequest)
+            .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                @Override
+                public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                    Log.i(TAG, "All location settings are satisfied.");
+
+                    //noinspection MissingPermission
+                    mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                                                                mLocationCallback,
+                                                                Looper.myLooper());
+
+                    displayLastLocation();
+                }
+            })
+            .addOnFailureListener(this, new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    int statusCode = ((ApiException) e).getStatusCode();
+                    switch (statusCode) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            Log.i(TAG, "Location settings are not satisfied. Attempting to upgrade " +
+                                    "location settings ");
+                            try {
+                                // Show the dialog by calling startResolutionForResult(), and check the
+                                // result in onActivityResult().
+                                ResolvableApiException rae = (ResolvableApiException) e;
+                                rae.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                            } catch (IntentSender.SendIntentException sie) {
+                                Log.i(TAG, "PendingIntent unable to execute request.");
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            String errorMessage = getString(R.string.settingsChangeUnavailable);
+                            Log.e(TAG, errorMessage);
+                            Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+                    }
+
+                    displayLastLocation();
+                }
+            });
+    }
+
+    /**
      * Provides a simple way of getting a device's location and is well suited for
      * applications that do not require a fine-grained location and that do not need location
      * updates. Gets the best and most recent location currently available, which may be null
@@ -78,26 +198,14 @@ public class MainActivity extends AppCompatActivity {
      * Note: this method should be called after location permission has been granted.
      */
     @SuppressWarnings("MissingPermission")
-    private void getLastLocation() {
-        mFusedLocationClient.getLastLocation()
-            .addOnCompleteListener(this, new OnCompleteListener<Location>() {
-                @Override
-                public void onComplete(@NonNull Task<Location> task) {
-                    if (task.isSuccessful() && task.getResult() != null) {
-                        mLastLocation = task.getResult();
-                        TextView mLatitudeText = (TextView) findViewById(R.id.LatitudeText);
-                        TextView mLongitudeText = (TextView) findViewById(R.id.LongitudeText);
+    private void displayLastLocation() {
+        TextView mLatitudeText = (TextView) findViewById(R.id.LatitudeText);
+        TextView mLongitudeText = (TextView) findViewById(R.id.LongitudeText);
 
-                        if (mLastLocation != null) {
-                            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-                            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
-                        }
-                    } else {
-                        Log.w(TAG, "getLastLocation:exception", task.getException());
-                        // TODO notify the user about this error
-                    }
-                }
-            });
+        if (mLastLocation != null) {
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
     }
 
     private void requestPermissions() {
