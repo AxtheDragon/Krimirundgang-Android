459374ca9dbd7c1249ba829ff72bf3eccacee211
Andi <ax.the.dragon@googlemail.com>
2017-08-20 17:40:29 +0200

    Made the StopInfo class parcelable

--- app/src/main/java/de/hottenstein/krimirundgang/StopInfo.java
+++ app/src/main/java/de/hottenstein/krimirundgang/StopInfo.java
@@ -1,12 +1,14 @@
 package de.hottenstein.krimirundgang;
 
 import android.location.Location;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by Andi on 20.08.2017.
  */
 
-public class StopInfo {
+public class StopInfo implements Parcelable {
     protected String title;
     protected String description;
     protected Location location;
@@ -14,4 +16,35 @@ public class StopInfo {
     protected static final String TITLE_PREFIX = "Title_";
     protected static final String DESCRIPTION_PREFIX = "DESCRIPTION_";
 
+
+    protected StopInfo(Parcel in) {
+        title = in.readString();
+        description = in.readString();
+        location = (Location) in.readValue(Location.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeValue(location);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<StopInfo> CREATOR = new Parcelable.Creator<StopInfo>() {
+        @Override
+        public StopInfo createFromParcel(Parcel in) {
+            return new StopInfo(in);
+        }
+
+        @Override
+        public StopInfo[] newArray(int size) {
+            return new StopInfo[size];
+        }
+    };
 }
