6fe672531f04d528cb107a6f22eb3f21a90cfc5e
Andi <ax.the.dragon@googlemail.com>
2017-08-20 16:12:28 +0200

    removed unused variable

--- app/src/main/java/de/hottenstein/krimirundgang/MainActivity.java
+++ app/src/main/java/de/hottenstein/krimirundgang/MainActivity.java
@@ -93,7 +93,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView tourCycler = (RecyclerView) findViewById(R.id.tourCycler);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         mSettingsClient = LocationServices.getSettingsClient(this);
         mLocationCallback = createLocationCallback();
