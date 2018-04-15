30e20b5524e7b1e47b1d0f73d003fb31c03b0d9c
Azure7111 <azure.diamond7111@googlemail.com>
2018-04-15 17:51:36 +0200

    Read tour info from file

--- app/src/main/java/de/hottenstein/krimirundgang/TourDetailActivity.java
+++ app/src/main/java/de/hottenstein/krimirundgang/TourDetailActivity.java
@@ -6,14 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import android.location.Location;
 import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class TourDetailActivity extends AppCompatActivity {
-
-    protected TourInfo MyTour = new TourInfo();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +32,11 @@ public class TourDetailActivity extends AppCompatActivity {
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(llm);
 
-        StopAdapter sa = new StopAdapter(MyTour.stopList, this);
+        TourInfo myTour = loadTour();
+
+        StopAdapter sa = new StopAdapter(myTour.stopList, this);
+        TextView title = (TextView) findViewById(R.id.title);
+        title.setText(myTour.title);
         recyclerView.setAdapter(sa);
     }
 
@@ -44,5 +55,40 @@ public class TourDetailActivity extends AppCompatActivity {
             result.add(si);
         }
         return result;
+    }
+
+    private TourInfo loadTour() {
+        InputStream inputStream = getResources().openRawResource(R.raw.example_tour);
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                sb.append(line + "\n");
+            }
+
+            String jsonString = sb.toString();
+
+            JSONObject jsonTour = new JSONObject(jsonString);
+            return new TourInfo(jsonTour.getString("Name"), new ArrayList<StopInfo>());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } finally {
+            if(inputStream != null){
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
     }
 }
